07465f879649bff77db5e6753188e9aca8914177
luohao <1223048059@qq.com>
2024-10-26 04:54:28 +0800

    feat: added user functionalities

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,14 @@
 import type home from '@/api/mockServeData/home'
 import type user from '@/api/mockServeData/user'
-import type { userDataType } from '@/api/mockServeData/user'
+import type { TGetUserListParams, TUser } from '@/api/mockServeData/user'
 import http from './axios'
 
 // 给每个接口设置返回类型
 type THomeData = ReturnType<typeof home.getStatisticalData>
 type TUserData = ReturnType<typeof user.getUserList>
 type TUserAddData = ReturnType<typeof user.createUser>
+type TUserEditData = ReturnType<typeof user.updateUser>
+type TUserDeleteData = ReturnType<typeof user.deleteUser>
 
 export const getData = () => {
 	return http.request<THomeData>({
@@ -15,7 +17,7 @@ export const getData = () => {
 	})
 }
 
-export const getUser = (params?: string) => {
+export const getUser = (params?: TGetUserListParams) => {
 	return http.request<TUserData>({
 		url: '/user/getUser',
 		method: 'get',
@@ -23,9 +25,25 @@ export const getUser = (params?: string) => {
 	})
 }
 
-export const addUser = (data: string) => {
+export const addUser = (data: TUser) => {
 	return http.request<TUserAddData>({
 		url: '/user/add',
+		method: 'post',
+		data,
+	})
+}
+
+export const editUser = (data: TUser) => {
+	return http.request<TUserEditData>({
+		url: '/user/edit',
+		method: 'post',
+		data,
+	})
+}
+
+export const deleteUser = (data: Pick<TUser, 'id'>) => {
+	return http.request<TUserDeleteData>({
+		url: '/user/del',
 		method: 'post',
 		data,
 	})
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,3 +1,5 @@
+import { addUser, deleteUser, editUser, getUser } from '@/api/index'
+import type { TUser } from '@/api/mockServeData/user'
 import {
 	Button,
 	DatePicker,
@@ -9,67 +11,117 @@ import {
 	Radio,
 	Table,
 } from 'antd'
-import { getUser } from '@/api/index'
-import type { userDataType } from '@/api/mockServeData/user'
-import type { RadioChangeEvent, DatePickerProps, TableColumnsType } from 'antd'
+import type {
+	DatePickerProps,
+	FormProps,
+	RadioChangeEvent,
+	TableColumnsType,
+} from 'antd'
 import dayjs from 'dayjs'
+
+type ValueOfObject<T extends object> = T extends Record<keyof T, infer V>
+	? V
+	: never
+
+const GENDER = {
+	FEMALE: 0,
+	MALE: 1,
+} as const
+
+const MODALSTATUS = {
+	CLOSE: 0,
+	OPEN: 1,
+} as const
 
 const User = () => {
 	//定义getUserList的参数
 	const [listData, setListData] = useState({
 		name: '',
 	})
-	const [tableData, setTableData] = useState([])
 	const [sex, setSex] = useState(0)
+	const [birth, setBirth] = useState<dayjs.Dayjs>()
+	const [tableData, setTableData] = useState<TUser[]>([])
 	//0代表新增 1代表编辑
-	const [modalType, setModalType] = useState(0)
+	const [modalType, setModalType] = useState<ValueOfObject<typeof MODALSTATUS>>(
+		MODALSTATUS.CLOSE,
+	)
 	const [isModalOpen, setIsModalOpen] = useState(false)
 	//创建form实例
-	const [form] = Form.useForm()
+	const [form] = Form.useForm<TUser>()
 
-	const handleClick = (type: string, rowData?: userDataType) => {
+	const handleClick = (type: 'add' | 'edit', rowData?: TUser) => {
 		setIsModalOpen(!isModalOpen)
 		if (type === 'add') {
 			setModalType(0)
-		} else if (type === 'edit') {
-			setModalType(1)
 		} else {
+			setModalType(1)
+			const cloneData = JSON.parse(JSON.stringify(rowData))
+			form.setFieldsValue({
+				...cloneData,
+				birth: dayjs(cloneData.birth),
+			})
 		}
 	}
 
-	const handleFinish = (e: any) => {
+	const handleFinish: FormProps<{ keyword: string }>['onFinish'] = (values) => {
+		if (values.keyword === listData.name) return
 		setListData({
-			name: e.name,
+			name: values.keyword,
 		})
 	}
+
+	// TODO 如何添加更多分页的数据？
 	const getTableData = () => {
-		getUser().then((data) => {
+		getUser(listData).then((data) => {
 			setTableData(data.list)
 		})
 	}
-	const handleDelete = (rowData: userDataType) => {}
+
+	const handleDelete = ({ id }: TUser) => {
+		deleteUser({ id }).then(() => {
+			getTableData()
+		})
+	}
+
 	const handleOk = () => {
 		//表单校验
 		form.validateFields().then((value) => {
 			//对表单日期进行单独的校验
-			value.birth = dayjs(value.birth).format('YYYY-MM-DD')
+			const newValue = {
+				...value,
+				birth: dayjs(value.birth).format('YYYY-MM-DD'),
+			}
 			// console.log(value, 'valid')
 			//调接口实现数据更新
-			// if()
+			if (modalType === MODALSTATUS.OPEN) {
+				editUser(newValue).then(() => {
+					handleCancel()
+					getTableData()
+				})
+			} else {
+				addUser(newValue).then(() => {
+					handleCancel()
+					getTableData()
+				})
+			}
 		})
 	}
+
 	const handleCancel = () => {
 		setIsModalOpen(false)
 	}
+
 	const changeSex = (e: RadioChangeEvent) => {
-		console.log('radio checked', e.target.value)
+		// console.log('radio checked', e.target.value)
 		setSex(e.target.value)
 	}
-	const changeBirth: DatePickerProps['onChange'] = (date, dateString) => {
-		console.log(date, dateString)
+
+	const changeBirth: DatePickerProps['onChange'] = (date) => {
+		// console.log(date, dateString)
+		setBirth(date)
 	}
 
-	const columns: TableColumnsType<userDataType> = [
+	const columns: TableColumnsType<TUser> = [
 		{
 			title: '姓名',
 			dataIndex: 'name',
@@ -82,7 +134,7 @@ const User = () => {
 			title: '性别',
 			dataIndex: 'sex',
 			render: (value: number) => {
-				return value ? '男' : '女'
+				return value === GENDER.MALE ? '男' : '女'
 			},
 		},
 		{
@@ -95,7 +147,7 @@ const User = () => {
 		},
 		{
 			title: '操作',
-			render: (rowData: userDataType) => {
+			render: (_, rowData) => {
 				return (
 					<div className="flex">
 						<Button
@@ -120,10 +172,12 @@ const User = () => {
 			},
 		},
 	]
+
 	useEffect(() => {
 		//获取用户信息
 		getTableData()
-	}, [])
+	}, [listData])
+
 	return (
 		<div className="flex flex-col">
 			<div className="flex w-full justify-between">
@@ -141,7 +195,12 @@ const User = () => {
 					</Form.Item>
 				</Form>
 			</div>
-			<Table columns={columns} dataSource={tableData} rowKey={'id'} />
+			<Table
+				style={{ marginTop: '10px' }}
+				columns={columns}
+				dataSource={tableData}
+				rowKey={'id'}
+			/>
 			<Modal
 				title={modalType ? '编辑' : '新增'}
 				open={isModalOpen}
@@ -156,6 +215,11 @@ const User = () => {
 					wrapperCol={{ span: 18 }}
 					labelAlign="left"
 				>
+					{modalType === MODALSTATUS.OPEN && (
+						<Form.Item name="id" hidden>
+							<Input />
+						</Form.Item>
+					)}
 					<Form.Item
 						label="姓名"
 						name="name"
@@ -184,12 +248,16 @@ const User = () => {
 						rules={[{ required: true, message: '请选择性别' }]}
 					>
 						<Radio.Group onChange={changeSex} value={sex}>
-							<Radio value={0}>男</Radio>
-							<Radio value={1}>女</Radio>
+							<Radio value={GENDER.MALE}>男</Radio>
+							<Radio value={GENDER.FEMALE}>女</Radio>
 						</Radio.Group>
 					</Form.Item>
 					<Form.Item label="出生日期" name="birth">
-						<DatePicker onChange={changeBirth} format="YYYY-MM-DD" />
+						<DatePicker
+							onChange={changeBirth}
+							value={birth}
+							format="YYYY-MM-DD"
+						/>
 					</Form.Item>
 					<Form.Item
 						label="地址"
